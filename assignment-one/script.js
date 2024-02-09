0931c4717d3bccc545b11b59a2c6a0726be042bb
ᴍᴏʜᴀᴍᴍᴇᴅ <132733865+tech-moh-logy@users.noreply.github.com>
2024-02-09 18:56:23 -0500

    Create script.js

---
+++ assignment-one/script.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    document.getElementById("demo").innerHTML = "Hello! You've just embarked on a wild adventure. Get ready to explore the fascinating world of zoo animals!";
+}
